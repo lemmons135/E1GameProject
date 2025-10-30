4dc8c69550ac89b10c0d03ad85144cae083ad34b
lemmons135 <lemmons135@gmail.com>
2025-10-30 00:14:17 -0700

    Added lukes land and movement

---
+++ Assets/Scripts/playerScript.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playerScript : MonoBehaviour
+{
+    // private member variables
+    Vector2 movementVector = Vector2.zero;
+    Rigidbody2D rb;
+    float speed;
+    bool isDashing = false;
+    float currentDashTime = 0.1f; //0.1 to avoid zero multiplication
+
+    // public member variables
+    [SerializeField] float walkingSpeed = 6f;
+    [SerializeField] float dashDuration = 0.5f;
+    [SerializeField] float dashMultiplier = 2f;
+
+    void Start()
+    {
+        dashDuration += currentDashTime; // to account for initial value
+        rb = GetComponent<Rigidbody2D>();
+        speed = walkingSpeed;
+    }
+
+    void OnMove(InputValue value)
+    {
+        movementVector = value.Get<Vector2>();
+    }
+
+    void OnJump()
+    {
+        if (!isDashing)
+        {
+            isDashing = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDashing) { runDashCountdown(); }
+
+        rb.linearVelocity = movementVector * speed;
+    }
+
+    void runDashCountdown()
+    {
+        if (currentDashTime <= dashDuration)
+        {
+            speed = dashMultiplier * currentDashTime * currentDashTime ;
+            currentDashTime += Time.deltaTime;
+        }
+        else
+        {
+            resetDash();
+        }
+    }
+
+    void resetDash()
+    {
+        isDashing = false;
+        speed = walkingSpeed;
+        currentDashTime = 0f;
+    }
+
+
+}
