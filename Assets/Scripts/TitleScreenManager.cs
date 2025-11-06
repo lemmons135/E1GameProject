e452c5dd006d5e84df90903f4600a5f2bc08d6b8
michaeeljay1010 <michaeeljay1010@gmail.com>
2025-11-05 16:55:38 -0800

    Working title screen ish

---
+++ Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,51 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    bool optionScreneOpen = false;
+    [SerializeField] GameObject optionsMenu;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void GoToNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("ooh im playing!");
+    }
+
+    public void OptionScrene()
+    {
+        if (optionScreneOpen)
+        {
+            optionsMenu.SetActive(false);
+            optionScreneOpen = false;
+            Debug.Log("ooh im options open!");
+        }
+        else
+        {
+            optionsMenu.SetActive(true);
+            optionScreneOpen = true;
+            Debug.Log("ooh im options close!");
+        }
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("ooh im quit!");
+
+    }
+
+}
