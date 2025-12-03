5e1c63f6ad61d9f31dc7fd29edc64abe292bd459
Dijitalex <alex.dong.reno@gmail.com>
2025-12-02 16:43:11 -0800

    Sssniperwolf's shaky cam effect is added

---
+++ Assets/Scripts/ShakyCam.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class ShakyCam : MonoBehaviour
+{
+    [SerializeField] CinemachineBasicMultiChannelPerlin screenshakeNoise;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        screenshakeNoise = FindAnyObjectByType<CinemachineBasicMultiChannelPerlin>();
+        StartCoroutine(ShakeScreen(10f,1f));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator ShakeScreen(float screenshakeTime, float amplitude){
+    Debug.Log("I am shaking my pants rn");
+    screenshakeNoise.AmplitudeGain = amplitude;
+    yield return new WaitForSeconds(screenshakeTime);
+    Debug.Log("I am no longer quivering no mo'");
+    screenshakeNoise.AmplitudeGain = 0f;
+    }
+}
+
+
